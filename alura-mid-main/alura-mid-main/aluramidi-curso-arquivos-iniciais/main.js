e96b820cd4f7bd9c31f4a6ccfa58dfec2e6f6ee3
b6okieMah <110109736+b6okieMah@users.noreply.github.com>
2023-11-10 08:50:03 -0300

    Add files via upload

---
+++ alura-mid-main/alura-mid-main/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,5 @@
+function tocaSom(idElementeoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+const listadeTeclas = document.querySelectorAll('.tecla');
+for (let contador = <listadeTeclas.length;contador++){}
